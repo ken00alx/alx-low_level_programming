1a981bd381fff47919fed3dffb53e5001bbb40fb
ken00alx <kennye933@gmail.com>
2022-07-12 20:10:28 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* write a program using printf function to
+ * print with \"proper grammar, but the outcome is a piece of art,\"
+ * not using the puts function.
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
