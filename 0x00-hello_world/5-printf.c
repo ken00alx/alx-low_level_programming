486d9497eb9e8bcad12b88f015ed44ee341398e4
ken00alx <kennye933@gmail.com>
2022-07-12 20:29:51 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,12 @@
  * not using the puts function.
  * Return 0
  */
-int main (void)
+int main(void)
 
 {
-	printf(%s,"with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+
+
+	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+		
+	return (0);
 }
