f4d565a2d42653d79b253ace8cec4a3224aa37a9
ken00alx <kennye933@gmail.com>
2022-07-12 20:42:33 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-/** write a program using printf function to
- * print with \"proper grammar, but the outcome is a piece of art,\"
- * not using the puts function.
- * Return 0
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
  */
+
 int main(void)
-
 {
-
-
-	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
-		
-	return (0);
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
