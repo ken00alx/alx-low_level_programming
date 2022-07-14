432e59d7c83f66b0f4d106affe431241ad9d8f3b
ken00alx <kennye933@gmail.com>
2022-07-14 03:41:45 -0700

    git push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 
 int main()
 {
-	fprintf( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fprintf("stderr", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
