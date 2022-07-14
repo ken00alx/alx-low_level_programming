a68b5786e6e8888d2a46fb67049731f884f3e896
alxse_kenny <108037115+ken00alx@users.noreply.github.com>
2022-07-14 12:06:56 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/** main - Entry point
+/** main-Entry point
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
-	fprintf("stderr", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
