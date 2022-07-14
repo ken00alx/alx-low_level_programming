2f9b746907d5f2eeddb76b279e45f1aac0f4f128
ken00alx <kennye933@gmail.com>
2022-07-14 06:08:37 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-/**
-* main - Entry point
-* Description: without using printf
-* Return: Always 1 (Sucess)
-*/
 
-int main(void)
-{
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
-}
