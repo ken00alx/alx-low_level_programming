c7a1921a5b5bd4afce074cb6196789b04ed464c6
ken00alx <kennye933@gmail.com>
2022-07-14 00:44:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+#include <stdio.h>
+
+ /** printing standard error stderr output
+  *  without the use of printf function
+  *  with a new line "\n"
+  */
+
+ int main(void)
+
+ {
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	
+	return(1);
+	
+}
