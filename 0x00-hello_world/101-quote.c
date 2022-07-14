729b59b3ec678754cb123c1f9a764eafd8145c99
ken00alx <kennye933@gmail.com>
2022-07-14 06:16:02 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/* *
+ * main - Entry puts
+ * descriptioni: to prints 
+ * return 0 (success)
+ */
+ 
+int main()
+
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+			sizeof(stderr, "and that piee of art is useful\" -Dora Korpar, 2015-10-19\n"));
+
+	return(1)
+}
