3e0722ee8ed2844f51834f3725a318d1949df621
alxse_kenny <108037115+ken00alx@users.noreply.github.com>
2022-07-14 08:48:46 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
-
-#!/bin/bash
 #include <stdio.h>
-
- /** printing standard error stderr output
-  *  without the use of printf function
-  *  with a new line "\n"
-  */
-
- int main(void)
-
- {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
-	
-	return(1);
-	
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+ 
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
