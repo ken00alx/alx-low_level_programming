f613252048c4143a4953844bd7880c0386e62f0f
ken00alx <kennye933@gmail.com>
2022-07-14 05:41:36 -0700

    betty 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* Description: 'prints without using printf'
+* Description: without using printf
 * Return: Always 0 (Sucess)
 */
 
 int main(void)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+sizeof(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
