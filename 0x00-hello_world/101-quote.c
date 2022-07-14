0be905ea35fe74ad1c975de6b650f111013bf34f
ken00alx <kennye933@gmail.com>
2022-07-14 05:28:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 1 
- */
+* main - Entry point
+* Description: 'prints without using printf'
+* Return: Always 0 (Sucess)
+*/
 
 int main(void)
-	{
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
