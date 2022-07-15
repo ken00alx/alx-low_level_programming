2dd1be6fc1789ad0909422b0255fe90e60d78207
ken00alx <kennye933@gmail.com>
2022-07-15 01:26:16 -0700

    101-Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entery point
  * Description: print task assigned
- * Return: val 0
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
- fprintf (stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        sizeof ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")); 
+	fprintf(stderr, "and that piece of art is useful\"
+		 - Dora Korpar, 2015-10-19\n"
+        sizeof ("and that piece of art is useful\"
+	       	- Dora Korpar, 2015-10-19\n")); 
 	return(1);
 }
