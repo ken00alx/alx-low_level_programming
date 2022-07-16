3d6d2a20eb79ecf843d2cee985dee7f925b4a6bd
ken00alx <kennye933@gmail.com>
2022-07-16 14:23:14 -0700

    Number_base_10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,16 @@
 /**
  * main - print number base 10 from 0 .
  *
- * Return: 0 .
+ * Return: Always 0 .
  */
 
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		printf("%d", num);
+
 	printf('\n');
 
 	return (0);
