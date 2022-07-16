75c326fa91470597ee4697c68ed63f6b0beb308c
ken00alx <kennye933@gmail.com>
2022-07-16 10:46:10 -0700

    Lowercase_Alphbet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * Author: kenny
+ * C-Language
+ */
+
+#include <stdio>
+/**
+ * main - alphabes in lowercase.
+ *
+ * Return: Always 0 .
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
