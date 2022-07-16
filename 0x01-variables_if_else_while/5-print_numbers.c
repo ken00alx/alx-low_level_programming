2fb5da43f4742df9a8e00c44a93675975d0f7aa4
ken00alx <kennye933@gmail.com>
2022-07-16 14:42:48 -0700

    Number_base_10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - print number base 10 from 0 .
+ * main - print number 10 from 0 
  *
- * Return: Always 0 .
+ * Return: Always 0 
  */
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-	printf("%d", num);
+	printf("%d", num'\n');
 	
 	printf('\n');
 
