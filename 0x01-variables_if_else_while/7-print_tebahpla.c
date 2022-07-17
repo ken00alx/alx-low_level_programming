79a548bd39037b689762fdbff2a66bb7c1dbba2d
ken00alx <kennye933@gmail.com>
2022-07-16 19:50:06 -0700

    Reverse_Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabet reverse mode
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (lettr = 'z'; letter <= 'a'' letter++)
+		putchar("%s", letter);
+
+	putchar('\n');
+
+	return (0);
+}
