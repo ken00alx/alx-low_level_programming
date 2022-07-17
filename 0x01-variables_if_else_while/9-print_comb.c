e2b7a9c7c74f8a2976935792fff2c374b2e9742e
ken00alx <kennye933@gmail.com>
2022-07-17 08:42:44 -0700

    print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,13 @@
 	int main(void)
 {
 	int num;
+
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
-		continue;								
-												putchar(',');
+			continue;								
+		putchar(',');
 		putchar(' ');
 	}
 
