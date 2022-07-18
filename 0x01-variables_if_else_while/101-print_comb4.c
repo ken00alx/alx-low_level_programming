5338b7a1ba9c3b052f5ccc46d4abc7f2a2d74e9d
ken00alx <kennye933@gmail.com>
2022-07-18 00:50:52 -0700

    104-comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 /**
  * main - Prints all possible combinations of three different digits,
- * in ascending order, separated by a comma followed by a space.
+ * 	in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -10,25 +10,24 @@
 
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-	
+		putchar((digit1 % 10) + '0');
+	}
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
+			putchar((ditit2 % 10) + '0');
+		}
 
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
+			}
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 
 				putchar(',');
 				putchar(' ');
-			}
-		}
-	}
-	
+		
 	putchar('\n');
 
 	return (0);
