7b5cdd9f6f5d12308041ede9e802ca44b4823e4b
ken00alx <kennye933@gmail.com>
2022-07-18 00:54:23 -0700

    104-comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,20 @@
 	int digit1, digit2, digit3;
 
 	for (digit1 = 0; digit1 < 8; digit1++)
-	{
+	
 		putchar((digit1 % 10) + '0');
-	}
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			putchar((ditit2 % 10) + '0');
-		}
+	
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		putchar((digit2 % 10) + '0');
+		
+	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+		putchar((digit3 % 10) + '0');
 
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit3 % 10) + '0');
-			}
+	if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-
-				putchar(',');
-				putchar(' ');
 		
 	putchar('\n');
 
