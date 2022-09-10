32777b1bb9483031c42483809c52636f2cfd89ea
ken00alx <kennye933@gmail.com>
2022-09-10 04:10:28 -0700

    fixed error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,6 @@ int main(void)
 	int num1, num2;
 
 
-
 	for (num1 = 0; num1 <= 98; num1++)
 
 	{
@@ -22,15 +21,12 @@ int main(void)
 		{
 
 			putchar((num1 / 10) + '0');
-
 			putchar((num1 % 10) + '0');
 
 			putchar(' ');
 
 			putchar((num2 / 10) + '0');
-
 			putchar((num2 % 10) + '0');
-
 
 
 			if (num1 == 98 && num2 == 99)
@@ -48,8 +44,4 @@ int main(void)
 	}
 
 	putchar ('\n');
-
-	return (0);
-
-
-
+	return(0);
