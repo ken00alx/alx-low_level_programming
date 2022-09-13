97e0f3363ae21bcca9a48c2d66080e26f1b2812a
ken00alx <kennye933@gmail.com>
2022-09-13 08:27:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifnder HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int -islower(int c);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
