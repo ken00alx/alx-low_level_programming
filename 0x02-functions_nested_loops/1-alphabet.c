3aa15ae21774c0d1ae7f0866160db102a658397d
007kenny <108037115+ken00alx@users.noreply.github.com>
2022-09-13 22:14:15 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,36 +1,21 @@
 #include "main.h"
 
-
-
 /**
  * print_alphabet - prints the english alphabet.
  *
  * Return: void.
  */
-
 void print_alphabet(void)
-
 {
-
 	char i;
 
-
-
 	i = 'a';
-
 	while (i <= 'z')
-
 	{
-
 		_putchar(i);
 
-
-
 		i++;
-
 	}
 
-
-
 	_putchar('\n');
-
+}
