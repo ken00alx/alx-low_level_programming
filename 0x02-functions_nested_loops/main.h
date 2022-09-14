a641f22275315cb7c0f543d13c2367c724399d95
ken00alx <kennye933@gmail.com>
2022-09-13 20:50:58 -0700

    update files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void print_tmes_table(int n);
+void print_times_table(int n);
 void num(int r, int n);
 #endif
