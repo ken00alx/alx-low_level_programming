2d5d024ba3821891aed8f9539ecb0e4fd209eeee
ken00alx <kennye933@gmail.com>
2022-09-19 08:36:32 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "msin.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int alx;
+
+	alx = *a;
+
+	*a = *b;
+	*b = alx;
+
