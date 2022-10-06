7b810430f7fde6a58d3b1fca2db2a17da14c4414
ken00alx <kennye933@gmail.com>
2022-10-06 11:12:51 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+0x0C. C - More malloc, free and memory allocatioin
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+int main(int argc, char *argv[]);
+
+#endif
