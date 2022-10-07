45c99de12d2c10e078b7ef7b7acb23010461f17f
ken00alx <kennye933@gmail.com>
2022-10-06 17:45:26 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,10 +12,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
-int main(int argc, char *argv[]);
 
 #endif
