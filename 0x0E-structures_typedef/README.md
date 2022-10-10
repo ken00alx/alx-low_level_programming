44380aa652a88f43a73a5d22e5330114fd31564f
007kenny <bunny_29eddy@yahoo.com>
2022-10-10 10:07:50 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+#STRUCTURE TYPEDEF OF A DOG#
+* Name and Owner of a Dog
+*Showing love to a dog.
+* Be a friend of the dog.
+* Take a dog for a walk.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - struct for dog info
+ * @name: type char
+ * @age: float type
+ * @owner: char type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
