15c53a7d4baff6b7b2ee5b687ffe753adccab7f8
ken00alx <bunny_29eddy@yahoo.com>
2022-10-10 18:02:28 -0700

    2-main.c file_name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	{
+		printf("%s\n", __BASE_FILE__);
+	}
+	return (0);
+}
