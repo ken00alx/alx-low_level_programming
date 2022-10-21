0089c05f5d084dd7c48a95ff8f6fffdf691bf259
007kenny <bunny_29eddy@yahoo.com>
2022-10-21 00:15:25 -0700

    &myMessage

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,37 +1,19 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
-
 void free_list(list_t *head)
 {
-	list_t *current_node;
+	list_t *temp;
 
-	while ((current_node = head) != NULL)
+	while (head)
 	{
-		head = head->next;
-		free(current_node->str);
-		free(current_node);
-	}
-}#include "lists.h"
-
-/**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
- */
-
-void free_list(list_t *head)
-{
-	list_t *current_node;
-
-	while ((current_node = head) != NULL)
-	{
-		head = head->next;
-		free(current_node->str);
-		free(current_node);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
